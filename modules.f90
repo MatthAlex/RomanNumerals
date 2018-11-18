5220d57e65c059711c2d610bd54b85788fd90c00
Matt Alexandrakis <matt.alexandrakis@gmail.com>
2018-11-18 20:24:09 +0000

    Clean Up
    - Removed deprecated code and unused variables
    - Removed unused debuging print statements
    - Removed keyboard input code
    - Removed-moved ASCII-creation routine and code to its own file
    
    - Naming scheme was made more consistent and understandable
    - Added comments and descriptions of functions and subroutines
    
    Signed-off-by: Matt Alexandrakis <matt.alexandrakis@gmail.com>

--- modules.f90
+++ modules.f90
@@ -1,18 +1,12 @@
       MODULE PARAMETERS
       implicit none
       
-      character(LEN=1) :: ValidArray(14)
-      character(LEN=2) :: ValidArrayPlus(6)
-      character(LEN=2) :: ValidArrayAll(13)
+      character(LEN=1) :: ValidCharacters(14) = (/ 'I','i', 'V','v', 'X','x', 'L','l', 'C','c', 'D','d', 'M','m' /) ! populate character array of 14 1-length characters, upper and lower
+      character(LEN=2) :: NumeralCharacters(13) = (/'M ','CM','D ','CD','C ','XC','L ','XL','X ','IX','V ','IV','I '/) ! populate all roman numerals in one array by descending order - will need trim
       
-      integer(kind=2) :: SingleRomanValues(7)= (/ 1, 5, 10, 50, 100, 500, 1000 /)   ! not used
-      integer(kind=2) :: DoubleRomanValues(6)= (/ 4, 9, 40, 90, 400, 900 /)         ! not used
-      integer(kind=2) :: RomanValues(13)= (/1,4,5,9,10,40,50,90,100,400,500,900,1000/)! not used
-      integer(kind=2) :: validCount(13)= (/3,1,1,1,3,1,1,1,3,1,1,1,3/)
-      integer(kind=1) :: testing = 0            ! this flag enables the creation of an ASCII file in the main program.
+      integer(kind=2) :: RomanNumberValues(13)= (/1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1/)
+      integer(kind=2) :: validCount(13)= (/3,1,1,1,3,1,1,1,3,1,1,1,3/)  ! the valid numeral count corresponds to Roman Numerals up to 3999 in value.
 
-      DATA ValidArray / 'I','i', 'V','v', 'X','x', 'L','l', 'C','c', 'D','d', 'M','m' / ! populate character array of 14 1-length characters, upper and lower
-      DATA ValidArrayPlus / 'IV', 'IX', 'XL', 'XC', 'CD', 'CM' / ! populate character array of 6 2-length characters
-      DATA ValidArrayAll /'I','IV','V','IX','X','XL','L','XC','C','CD','D','CM','M' / ! populate all roman numerals in one array - will need trim
+      logical         :: debugMode = .TRUE.
 
       END MODULE
