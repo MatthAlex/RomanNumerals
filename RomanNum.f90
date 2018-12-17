9ef9b699e0605e53739a1289cf57be8e28f228f6
MatthAlex <>
2018-12-17 11:25:21 +0000

    Main program updated
    - Two additional checks for the number of arguments and final conversion added
    - converted variables and subroutines to lowercase
    - changed names to Camel_Case instead of CamelCase for consistency
    - Sanitized comments, by purging self explanatory ones and moving everything before the execution block

--- RomanNum.f90
+++ RomanNum.f90
@@ -1,39 +1,55 @@
-      program numerals
-      use parameters, only: debugMode
-      use routines
-      implicit none
-      
-      integer(kind=4)               :: Number_1, Number_2, NumberSum
-      character(LEN=20)             :: command_line_argument_1, command_line_argument_2
-      character(LEN=:), allocatable :: Numeral_1, Numeral_2, NumeralSum
+	! This program takes the input of two Roman Numerals and produces and outputs the sum as a Numeral.
+	! The valid Numeral values are I, V, X, L, C, D, and M and their lowercase.
+	! Invalid input includes all other ASCII characters.
+	  
+	program numerals
+	use parameters, only: debugMode
+	use routines
+	implicit none
+	
+	integer(kind=4)			     		:: Number_1, Number_2, Number_Sum, Number_Check_Sum
+	character(LEN=20)		 	 		:: command_line_argument_1, command_line_argument_2
+	character(LEN=:), allocatable 	:: Numeral_1, Numeral_2, Numeral_Sum
 
-      
+	! Check argument count and abort if different than 2
+	argument_count = GET_ARGUMENT_COUNT()
+	if (argument_count .ne. 2) then
+		write(*,*) 'ERROR: Only two command line arguments allowed.'
+		stop
+	end if 
+	
+	! Read arguments #1 and #2 from the command line 
+	call GET_COMMAND_ARGUMENT(1, command_line_argument_1) 
+	call GET_COMMAND_ARGUMENT(2, command_line_argument_2) 
+	
+	! Adjust to the left and trim trailing spaces of the strings, and then check for invalid ASCII characters
+	call buffer_to_string(command_line_argument_1, Numeral_1) 
+	call check_string_valid(Numeral_1) 
+	
+	call buffer_to_string(command_line_argument_2, Numeral_2) 
+	call check_string_valid(Numeral_2) 
+	
+	! Translate numerals to numbers
+	Number_1 = numeral_to_number(Numeral_1)
+	Number_2 = numeral_to_number(Numeral_2)
 
-      ! This program takes the input of two Roman Numerals and produces and outputs the sum as a Numeral.
-      ! The valid Numeral values are I, V, X, L, C, D, and M and their lowercase.
-      ! Invalid input includes all other ASCII characters.
-      
-      call GET_COMMAND_ARGUMENT(1, command_line_argument_1)   ! Read argument #1 
-      call GET_COMMAND_ARGUMENT(2, command_line_argument_2)   ! and #2 from the command line, and assign it to command_line_argument#
+	! Add the numbers 
+	Number_Sum = Number_1 + Number_2
 
-      call BUFFER_TO_STRING(command_line_argument_1, Numeral_1)   ! Adjust to the left and trim trailing spaces from the string 
-      call CHECK_STRING_VALID(Numeral_1)   ! Check the input for invalid ASCII characters, return true or STOP the program
-      
-      call BUFFER_TO_STRING(command_line_argument_2, Numeral_2)   ! Adjust to the left and trim trailing spaces from the string
-      call CHECK_STRING_VALID(Numeral_2)   ! Check the input for invalid ASCII characters, return true or STOP the program
-      
-      Number_1 = NUMERAL_TO_NUMBER(Numeral_1)
-      Number_2 = NUMERAL_TO_NUMBER(Numeral_2)
+	! debug output 
+	if (debugMode) write(*,*) Number_1, Number_2, Number_Sum
 
-      NumberSum = Number_1 + Number_2
+	! Translate numeral to number
+	Numeral_Sum = number_to_numeral(Number_Sum)
 
-      if (debugMode) write(*,*) Number_1, Number_2, NumberSum
+	! Check that the conversion holds true
+	Number_Check_Sum = numeral_to_number(Numeral_Sum)
+	if (Number_Check_Sum.ne.Number_Sum) then
+		write(*,*) 'ERROR: The sum and numeral are not equal.'
+		stop
+	end if 
+	
+	write(*,*) Numeral_Sum
+	
 
-      NumeralSum = NUMBER_TO_NUMERAL(NumberSum)
-
-      write(*,*) NumeralSum
-      
-
-      end program numerals
-
-      
+	end program numerals
