9ad0e3fdcdd5c233baf9d9fec0d8f634b2aa87b1
MatthAlex <>
2018-12-17 15:56:27 +0000

    Realigned arrays to nominal order. Added <parameter> to non-changing arrays.

--- parameters.f90
+++ parameters.f90
@@ -1,12 +1,19 @@
-      module parameters
-      implicit none
-      
-      character(LEN=1) :: ValidCharacters(14) = (/ 'I','i', 'V','v', 'X','x', 'L','l', 'C','c', 'D','d', 'M','m' /) ! populate character array of 14 1-length characters, upper and lower
-      character(LEN=2) :: NumeralCharacters(13) = (/'M ','CM','D ','CD','C ','XC','L ','XL','X ','IX','V ','IV','I '/) ! populate all roman numerals in one array by descending order - will need trim
-      
-      integer(kind=2) :: RomanNumberValues(13)= (/1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1/)
-      integer(kind=2) :: validCount(13)= (/3,1,1,1,3,1,1,1,3,1,1,1,3/)  ! the valid numeral count corresponds to Roman Numerals up to 3999 in value.
+	module parameters
+	implicit none
 
-      logical         :: debugMode = .FALSE.
+	! populate character array of 7 1-length characters, uppercase only
+	character(LEN=1), parameter :: ValidCharacters(7) = [ 'I', 'V', 'X', 'L', 'C', 'D', 'M' ]
+	! populate all roman numerals in one array by descending order - will need trim
+	character(LEN=2), parameter :: NumeralCharacters(13) = ['I ','IV','V ','IX','X ','XL','L ','XC','C ','CD','D ','CM','M ']
+	!character(LEN=2) :: NumeralCharacters(13) = ['M ','CM','D ','CD','C ','XC','L ','XL','X ','IX','V ','IV','I ']
+	! equivalent numerical values to NumeralCharacters
+	integer, parameter :: RomanNumberValues(13)= [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+	! integer(kind=2) :: RomanNumberValues(13)= (/1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1/)
 
-      end module parameters
+	! the valid numeral count corresponds to Roman Numerals up to 3999 in value.
+	integer, parameter :: validCount(13)= [3,1,1,1,3,1,1,1,3,1,1,1,3]
+
+	! enable to print debug information
+	logical	 :: debugMode = .FALSE.
+
+	end module parameters
