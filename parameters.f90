651eb1e44a33bd31228b80003efadad847f174dd
MatthAlex <>
2018-12-17 16:10:55 +0000

    Removed commented out code

--- parameters.f90
+++ parameters.f90
@@ -3,12 +3,12 @@
 
 	! populate character array of 7 1-length characters, uppercase only
 	character(LEN=1), parameter :: ValidCharacters(7) = [ 'I', 'V', 'X', 'L', 'C', 'D', 'M' ]
+
 	! populate all roman numerals in one array by descending order - will need trim
 	character(LEN=2), parameter :: NumeralCharacters(13) = ['I ','IV','V ','IX','X ','XL','L ','XC','C ','CD','D ','CM','M ']
-	!character(LEN=2) :: NumeralCharacters(13) = ['M ','CM','D ','CD','C ','XC','L ','XL','X ','IX','V ','IV','I ']
+
 	! equivalent numerical values to NumeralCharacters
 	integer, parameter :: RomanNumberValues(13)= [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
-	! integer(kind=2) :: RomanNumberValues(13)= (/1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1/)
 
 	! the valid numeral count corresponds to Roman Numerals up to 3999 in value.
 	integer, parameter :: validCount(13)= [3,1,1,1,3,1,1,1,3,1,1,1,3]
