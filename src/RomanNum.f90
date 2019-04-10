3acaaa9ebcb3e7094c25841de255e3c59e55760c
MatthAlex <>
2019-04-10 10:45:16 +0100

    Tabs fix
    * Changed tabs into spaces, indentation to 2 spaces, removed whitespace
    
    * 3-space indentation

--- src/RomanNum.f90
+++ src/RomanNum.f90
@@ -1,58 +1,58 @@
-	!> This program takes the input of two Roman Numerals and produces and outputs the sum as a Numeral.
-	!> The valid Numeral values are I, V, X, L, C, D, and M and their lowercase.
-	!> Invalid input includes all other ASCII characters.
-	  
-	program numerals
-	use parameters, only: debugMode
-	use routines
-	implicit none
-	
-	integer	:: Number_1 , Number_2	!! Store the Numerals in the equivalent numerical form
-	integer :: Number_Sum, Number_Check_Sum	!! Store the sum of the two numbers	!! Store the final Numeral result in equivalent number 
-	integer :: argument_count	!! Number of command line arguments
-	character(LEN=20)	:: command_line_argument_1, command_line_argument_2	!! Store the command line input arguments
-	character(LEN=:), allocatable	:: Numeral_1, Numeral_2, Numeral_Sum	!! Store the numerals in a string of deferred length
+   !> This program takes the input of two Roman Numerals and produces and outputs the sum as a Numeral.
+   !> The valid Numeral values are I, V, X, L, C, D, and M and their lowercase.
+   !> Invalid input includes all other ASCII characters.
 
-	! Check argument count and abort if different than 2
-	argument_count = COMMAND_ARGUMENT_COUNT()
-	if (argument_count .ne. 2) then
-		write(*,*) 'ERROR: Only two command line arguments allowed.'
-		stop
-	end if
-	
-	! Read arguments #1 and #2 from the command line
-	call GET_COMMAND_ARGUMENT(1, command_line_argument_1)
-	call GET_COMMAND_ARGUMENT(2, command_line_argument_2)
-	
-	! Adjust to the left and trim trailing spaces of the strings, and then check for invalid ASCII characters
-	call buffer_to_string(command_line_argument_1, Numeral_1)
-	call verify_string(Numeral_1)
-	
-	call buffer_to_string(command_line_argument_2, Numeral_2)
-	call verify_string(Numeral_2)
-	
-	! Translate numerals to numbers
-	Number_1 = numeral_to_number(Numeral_1)
-	Number_2 = numeral_to_number(Numeral_2)
+   program numerals
+   use parameters, only: debugMode
+   use routines
+   implicit none
 
-	! Add the numbers
-	Number_Sum = Number_1 + Number_2
+   integer   :: Number_1 , Number_2   !! Store the Numerals in the equivalent numerical form
+   integer :: Number_Sum, Number_Check_Sum   !! Store the sum of the two numbers   !! Store the final Numeral result in equivalent number
+   integer :: argument_count   !! Number of command line arguments
+   character(LEN=20)   :: command_line_argument_1, command_line_argument_2   !! Store the command line input arguments
+   character(LEN=:), allocatable   :: Numeral_1, Numeral_2, Numeral_Sum   !! Store the numerals in a string of deferred length
 
-	! debug output
-	if (debugMode) write(*,*) Number_1, Number_2, Number_Sum
+   ! Check argument count and abort if different than 2
+   argument_count = COMMAND_ARGUMENT_COUNT()
+   if (argument_count .ne. 2) then
+      write(*,*) 'ERROR: Only two command line arguments allowed.'
+      stop
+   end if
 
-	! Translate numeral to number
-	call number_to_numeral(Number_Sum, Numeral_Sum)
-	if (debugMode) write(*,*) 'Numeral_Sum=',Number_Sum, ' and LEN=',LEN(Numeral_Sum)
+   ! Read arguments #1 and #2 from the command line
+   call GET_COMMAND_ARGUMENT(1, command_line_argument_1)
+   call GET_COMMAND_ARGUMENT(2, command_line_argument_2)
 
-	! Check that the conversion holds true
-	Number_Check_Sum = numeral_to_number(Numeral_Sum)
-	if (Number_Check_Sum.ne.Number_Sum) then
-		write(*,*) 'ERROR: The sum and numeral are not equal.'
-		stop
-	end if
-	
-	write(*,*) Numeral_Sum
-	
+   ! Adjust to the left and trim trailing spaces of the strings, and then check for invalid ASCII characters
+   call buffer_to_string(command_line_argument_1, Numeral_1)
+   call verify_string(Numeral_1)
 
-	end program numerals
+   call buffer_to_string(command_line_argument_2, Numeral_2)
+   call verify_string(Numeral_2)
+
+   ! Translate numerals to numbers
+   Number_1 = numeral_to_number(Numeral_1)
+   Number_2 = numeral_to_number(Numeral_2)
+
+   ! Add the numbers
+   Number_Sum = Number_1 + Number_2
+
+   ! debug output
+   if (debugMode) write(*,*) Number_1, Number_2, Number_Sum
+
+   ! Translate numeral to number
+   call number_to_numeral(Number_Sum, Numeral_Sum)
+   if (debugMode) write(*,*) 'Numeral_Sum=',Number_Sum, ' and LEN=',LEN(Numeral_Sum)
+
+   ! Check that the conversion holds true
+   Number_Check_Sum = numeral_to_number(Numeral_Sum)
+   if (Number_Check_Sum.ne.Number_Sum) then
+      write(*,*) 'ERROR: The sum and numeral are not equal.'
+      stop
+   end if
+
+   write(*,*) Numeral_Sum
+
+
+   end program numerals
--- src/parameters.f90
+++ src/parameters.f90
@@ -1,20 +1,20 @@
-	!> Contains all constants used in the program
-	module parameters
-	implicit none
+   !> Contains all constants used in the program
+   module parameters
+   implicit none
 
-	!> populate character array of 7 1-length characters, uppercase only
-	character(LEN=1), parameter :: ValidCharacters(7) = [ 'I', 'V', 'X', 'L', 'C', 'D', 'M' ]
+   !> populate character array of 7 1-length characters, uppercase only
+   character(LEN=1), parameter :: ValidCharacters(7) = [ 'I', 'V', 'X', 'L', 'C', 'D', 'M' ]
 
-	!> populate all roman numerals in one array by descending order - will need trim
-	character(LEN=2), parameter :: NumeralCharacters(13) = ['I ','IV','V ','IX','X ','XL','L ','XC','C ','CD','D ','CM','M ']
+   !> populate all roman numerals in one array by descending order - will need trim
+   character(LEN=2), parameter :: NumeralCharacters(13) = ['I ','IV','V ','IX','X ','XL','L ','XC','C ','CD','D ','CM','M ']
 
-	!> equivalent numerical values to NumeralCharacters
-	integer, parameter :: RomanNumberValues(13)= [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+   !> equivalent numerical values to NumeralCharacters
+   integer, parameter :: RomanNumberValues(13)= [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
 
-	!> the valid numeral count corresponds to Roman Numerals up to 3999 in value.
-	integer, parameter :: validCount(13)= [3,1,1,1,3,1,1,1,3,1,1,1,3]
+   !> the valid numeral count corresponds to Roman Numerals up to 3999 in value.
+   integer, parameter :: validCount(13)= [3,1,1,1,3,1,1,1,3,1,1,1,3]
 
-	!> enable to print debug information
-	logical	 :: debugMode = .FALSE.
+   !> enable to print debug information
+   logical :: debugMode = .FALSE.
 
-	end module parameters
+   end module parameters
